fdcec1c41774b2ade8d76f02aa282c9f07581273
misbahuddin110399 <misbahuddin110399@gmail.com>
2025-10-16 05:51:16 -0400

    Add local files created in git bash

---
+++ app.js
@@ -0,0 +1,7 @@
+//my first local javascript file
+console.log("Hello from local computer");
+console.log("this file will be pushed to github")
+
+function welcome(){
+  return "Welcome to github Learning";
+}
